05a45706286b078498b6dd8e6e34cac02bd667a7
akshayaa1010 <akshayaamaharishi@gmail.com>
2025-10-03 12:49:23 +0530

    Added 1 file to email-repo

---
+++ data/groundwaterData.ts
@@ -0,0 +1,92 @@
+export const groundwaterData = `State,District,Area,Year,Recharge_HAM,Rainfall_mm,Annual_Extractable_GW_HAM,WaterLevel_m,Soil_type,Status
+Tamil Nadu,Chennai,MAMBALAM,2019-2020,78.49,2577.77,70.64,8.2,Sandy Clay,Over Exploited
+Tamil Nadu,Chennai,EGMORE,2019-2020,155.12,473.1,139.6,8.2,Sandy Clay,Semi-Critical
+Tamil Nadu,Chennai,MYLAPORE,2019-2020,63.75,936.78,57.37,8.2,Sandy Clay,Safe
+Tamil Nadu,Chennai,MAMBALAM,2020-2021,No data found,No data found,No data found,No data found,Sandy Clay,No data found
+Tamil Nadu,Chennai,EGMORE,2020-2021,No data found,No data found,No data found,No data found,Sandy Clay,No data found
+Tamil Nadu,Chennai,MYLAPORE,2020-2021,No data found,No data found,No data found,No data found,Sandy Clay,No data found
+Tamil Nadu,Chennai,MAMBALAM,2021-2022,34.83,1577.77,31.35,9.5,Sandy Clay,Over Exploited
+Tamil Nadu,Chennai,EGMORE,2021-2022,339.37,"1,223.10",305.43,9.5,Sandy Clay,Over Exploited
+Tamil Nadu,Chennai,MYLAPORE,2021-2022,332.73,"1,377.77",299.46,9.5,Sandy Clay,Over Exploited
+Tamil Nadu,Chennai,MAMBALAM,2022-2023,82.06,"1,225.83",73.85,10.2,Sandy Clay,Over Exploited
+Tamil Nadu,Chennai,EGMORE,2022-2023,308.46,"1,223.10",281.59,10.2,Sandy Clay,Over Exploited
+Tamil Nadu,Chennai,MYLAPORE,2022-2023,347.43,1320.78,312.69,10.2,Sandy Clay,Over Exploited
+Tamil Nadu,Chennai,MAMBALAM,2023-2024,98.09,"1,486.53",88.27,10.7,Sandy Clay,Over Exploited
+Tamil Nadu,Chennai,EGMORE,2023-2024,373.13,"1,494.90",340.56,10.7,Sandy Clay,Over Exploited
+Tamil Nadu,Chennai,MYLAPORE,2023-2024,385.64,1439.1,347.07,10.7,Sandy Clay,Over Exploited
+Tamil Nadu,Chennai,MAMBALAM,2024-2025,108.63,1484.59,99.25,11.3,Sandy Clay,Over Exploited
+Tamil Nadu,Chennai,EGMORE,2024-2025,359.4,"1,489.38",328.05,11.3,Sandy Clay,Over Exploited
+Tamil Nadu,Chennai,MYLAPORE,2024-2025,378.63,"1,466.67",340.76,11.3,Sandy Clay,Over Exploited
+Tamil Nadu,Kanchipuram,Walajabad,2019-2020,2094.52,1307,1885.07,7.8,Clay Loam,Over Exploited
+Tamil Nadu,Kanchipuram,Sriperumbudur,2019-2020,2069.63,1224,1862.67,7.8,Clay Loam,Safe
+Tamil Nadu,Kanchipuram,Kundrathur,2019-2020,1573.46,1615,1416.11,7.8,Clay Loam,Safe
+Tamil Nadu,Kanchipuram,Walajabad,2020-2021,No data found,No data found,No data found,No data found,Clay Loam,No data found
+Tamil Nadu,Kanchipuram,Sriperumbudur,2020-2021,No data found,No data found,No data found,No data found,Clay Loam,No data found
+Tamil Nadu,Kanchipuram,Kundrathur,2020-2021,No data found,No data found,No data found,No data found,Clay Loam,No data found
+Tamil Nadu,Kanchipuram,Walajabad,2021-2022,2107.56,1307,1896.8,9,Clay Loam,Critical
+Tamil Nadu,Kanchipuram,Sriperumbudur,2021-2022,"2,346.03","1,224.00","2,111.43",9,Clay Loam,Safe
+Tamil Nadu,Kanchipuram,Kundrathur,2021-2022,"1,571.75","1,615.00","1,414.57",9,Clay Loam,Safe
+Tamil Nadu,Kanchipuram,Walajabad,2022-2023,"7,077.93","1,192.34","6,502.14",9.9,Clay Loam,Safe
+Tamil Nadu,Kanchipuram,Sriperumbudur,2022-2023,"11,205.12","1,265.56","10,208.62",9.9,Clay Loam,Safe
+Tamil Nadu,Kanchipuram,Kundrathur,2022-2023,"6,928.46","1,410.46","6,235.61",9.9,Clay Loam,Safe
+Tamil Nadu,Kanchipuram,Walajabad,2023-2024,"7,233.27","1,192.34","6,636.59",10.7,Clay Loam,Safe
+Tamil Nadu,Kanchipuram,Sriperumbudur,2023-2024,"10,905.59","1,265.56","9,815.04",10.70,Clay Loam,Safe
+Tamil Nadu,Kanchipuram,Kundrathur,2023-2024,"6,587.41","1,410.46","5,928.64",10.7,Clay Loam,Safe
+Tamil Nadu,Kanchipuram,Walajabad,2024-2025,"6,885.23","1,251.54","6,196.69",11.7,Clay Loam,Safe
+Tamil Nadu,Kanchipuram,Sriperumbudur,2024-2025,"11,146.76","1,223.86","10,032.09",11.7,Clay Loam,Safe
+Tamil Nadu,Kanchipuram,Kundrathur,2024-2025,"1,987.86","1,567.70",193.1,11.7,Clay Loam,Safe
+Tamil Nadu,Madurai,ANDIPATTI,2019-2020,"1,959.27","1,227.69","1,763.35",6.5,Red Loam,Safe
+Tamil Nadu,Madurai,USILAMPATTI,2019-2020,"2,451.83","1,066.98","2,206.64",6.5,Red Loam,Safe
+Tamil Nadu,Madurai,ELUMALAI,2019-2020,"2,126.79",783.51,"1,914.11",6.5,Red Loam,Safe
+Tamil Nadu,Madurai,ANDIPATTI,2020-2021,No data found,No data found,No data found,No data found,Red Loam,No data found
+Tamil Nadu,Madurai,USILAMPATTI,2020-2021,No data found,No data found,No data found,No data found,Red Loam,No data found
+Tamil Nadu,Madurai,ELUMALAI,2020-2021,No data found,No data found,No data found,No data found,Red Loam,No data found
+Tamil Nadu,Madurai,ANDIPATTI,2021-2022,"1,966.54","1,227.69","1,769.89",7.9,Red Loam,Critical
+Tamil Nadu,Madurai,USILAMPATTI,2021-2022,"3,054.04","1,066.98","2,748.63",7.9,Red Loam,Critical
+Tamil Nadu,Madurai,ELUMALAI,2021-2022,"2,304.03",783.51,"2,073.63",7.9,Red Loam,Critical
+Tamil Nadu,Madurai,ANDIPATTI,2022-2023,"6,022.47","1,218.55","5,420.22",8.5,Red Loam,Semi-Critical
+Tamil Nadu,Madurai,USILAMPATTI,2022-2023,"9,564.86","1,066.98","8,703.19",8.5,Red Loam,Semi-Critical
+Tamil Nadu,Madurai,ELUMALAI,2022-2023,"6,279.90",687.85,"5,718.45",8.5,Red Loam,Semi-Critical
+Tamil Nadu,Madurai,ANDIPATTI,2023-2024,"5,755.52","1,218.55","5,179.97",9.3,Red Loam,Critical
+Tamil Nadu,Madurai,USILAMPATTI,2023-2024,"8,988.26",922.54,"8,177.86",9.3,Red Loam,Critical
+Tamil Nadu,Madurai,ELUMALAI,2023-2024,"9,012.43",819.12,"8,169.56",9.3,Red Loam,Critical
+Tamil Nadu,Madurai,ANDIPATTI,2024-2025,"9,646.97",859.7,"8,682.26",10.2,Red Loam,Critical
+Tamil Nadu,Madurai,USILAMPATTI,2024-2025,"10,052.57",964.69,"9,047.33",10.2,Red Loam,Critical
+Tamil Nadu,Madurai,ELUMALAI,2024-2025,"10,195.16",908.46,"9,242.90",10.2,Red Loam,Critical
+Tamil Nadu,Tiruvallur,Avadi,2019-2020,678.2,1386.7,644.23,8,Alluvial,Over Exploited
+Tamil Nadu,Tiruvallur,Gummudipoondi,2019-2020,3655.29,1204.74,3472.51,8,Alluvial,Safe
+Tamil Nadu,Tiruvallur,Poonamallee,2019-2020,529.91,1413.7,476.91,8,Alluvial,Over Exploited
+Tamil Nadu,Tiruvallur,Avadi,2020-2021,No data found,No data found,No data found,No data found,Alluvial,No data found
+Tamil Nadu,Tiruvallur,Gummudipoondi,2020-2021,No data found,No data found,No data found,No data found,Alluvial,No data found
+Tamil Nadu,Tiruvallur,Poonamallee,2020-2021,No data found,No data found,No data found,No data found,Alluvial,No data found
+Tamil Nadu,Tiruvallur,Avadi,2021-2022,563.69,1386.7,535.69,9.4,Alluvial,Semi Critical
+Tamil Nadu,Tiruvallur,Gummudipoondi,2021-2022,3816.21,1204.74,3434.59,9.4,Alluvial,Safe
+Tamil Nadu,Tiruvallur,Poonamallee,2021-2022,521.54,1413.7,469.38,9.4,Alluvial,Critical
+Tamil Nadu,Tiruvallur,Avadi,2022-2023,4489.25,1308.01,4121.7,10.2,Alluvial,Semi Critical
+Tamil Nadu,Tiruvallur,Gummudipoondi,2022-2023,13319.51,1204.74,11987.55,10.2,Alluvial,Safe
+Tamil Nadu,Tiruvallur,Poonamallee,2022-2023,4165.44,1330.89,3858.8,10.2,Alluvial,Safe
+Tamil Nadu,Tiruvallur,Avadi,2023-2024,4613.26,1358.15,4151.94,10.9,Alluvial,Semi Critical
+Tamil Nadu,Tiruvallur,Gummudipoondi,2023-2024,11012.45,1291,9911.2,10.9,Alluvial,Safe
+Tamil Nadu,Tiruvallur,Poonamallee,2023-2024,4151.03,1337.32,3845.48,10.9,Alluvial,Safe
+Tamil Nadu,Tiruvallur,Avadi,2024-2025,4457.35,1331.11,4054.24,11.8,Alluvial,Semi Critical
+Tamil Nadu,Tiruvallur,Gummudipoondi,2024-2025,10235.45,1308.55,9340.05,11.8,Alluvial,Safe
+Tamil Nadu,Tiruvallur,Poonamallee,2024-2025,3863.86,1519.64,3642.37,11.8,Alluvial,Safe
+Tamil Nadu,Ariyalur,Udayarpalayam,2019-2020,1945.81,1438,1844.13,7,Black Cotton,Safe
+Tamil Nadu,Ariyalur,Sendurai,2019-2020,1487.54,1105,1409.79,7,Black Cotton,Semi Critical
+Tamil Nadu,Ariyalur,Andimadam,2019-2020,2736.27,1558,2462.65,7,Black Cotton,Safe
+Tamil Nadu,Ariyalur,Udayarpalayam,2020-2021,No data found,No data found,No data found,No data found,Black Cotton,No data found
+Tamil Nadu,Ariyalur,Sendurai,2020-2021,No data found,No data found,No data found,No data found,Black Cotton,No data found
+Tamil Nadu,Ariyalur,Andimadam,2020-2021,No data found,No data found,No data found,No data found,Black Cotton,No data found
+Tamil Nadu,Ariyalur,Udayarpalayam,2021-2022,1600.69,1038,1516.26,8.5,Black Cotton,Safe
+Tamil Nadu,Ariyalur,Sendurai,2021-2022,1197.83,905,1134.57,8.5,Black Cotton,Semi Critical
+Tamil Nadu,Ariyalur,Andimadam,2021-2022,1996.51,1329,1796.86,8.5,Black Cotton,Safe
+Tamil Nadu,Ariyalur,Udayarpalayam,2022-2023,13160.61,1010.28,11997.47,9.2,Black Cotton,Safe
+Tamil Nadu,Ariyalur,Sendurai,2022-2023,5560.99,905,5004.88,9.2,Black Cotton,Safe
+Tamil Nadu,Ariyalur,Andimadam,2022-2023,3904.4,1038,3599.53,9.2,Black Cotton,Semi Critical
+Tamil Nadu,Ariyalur,Udayarpalayam,2023-2024,13032.79,974.49,11980.35,9.8,Black Cotton,Safe
+Tamil Nadu,Ariyalur,Sendurai,2023-2024,5639.53,861.39,5123.59,9.8,Black Cotton,Safe
+Tamil Nadu,Ariyalur,Andimadam,2023-2024,3347.74,1038,3012.98,9.8,Black Cotton,Semi Critical
+Tamil Nadu,Ariyalur,Udayarpalayam,2024-2025,13212.69,1037.31,12085.73,10.4,Black Cotton,Safe
+Tamil Nadu,Ariyalur,Sendurai,2024-2025,6289.65,942.96,5660.69,10.4,Black Cotton,Safe
+Tamil Nadu,Ariyalur,Andimadam,2024-2025,3888.79,1180.25,3587.78,10.4,Black Cotton,Semi Critical
+`;
